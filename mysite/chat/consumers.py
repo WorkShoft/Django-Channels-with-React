0b3516d9a484857b3f78867cacd8b87738236244
Mikel Losada <mikel.losada@gmail.com>
2020-05-28 10:38:40 +0200

    Add docstrings and refactor utc_time

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -7,6 +7,11 @@ from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        """
+        Connect to a chat room
+        Spaces are replaced like this: 'My new room' -> 'My_new_room'
+        """
+        
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_name = self.room_name.replace(' ', '_')
         self.room_group_name = 'chat_%s' % self.room_name
@@ -20,34 +25,43 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
 
     def disconnect(self, close_code):
-        # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket
+
     def receive(self, text_data):
+        """
+        Receive a message and broadcast it to a room group
+        UTC time is included so the client can display it in each user's local time
+        """
+        
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        utc_time = datetime.datetime.now(datetime.timezone.utc)
+        utc_time = utc_time.isoformat()
 
-        # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'utc_time': utc_time,
             }
         )
 
-    # Receive message from room group
     def chat_message(self, event):
+        """
+        Receive a broadcast message and send it over a websocket
+        """
+        
         message = event['message']
-        utc_time = datetime.datetime.now(datetime.timezone.utc)
+        utc_time = event['utc_time']
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
             'message': message,
-            'utc_time': utc_time.isoformat(),
+            'utc_time': utc_time,
         }))
  
