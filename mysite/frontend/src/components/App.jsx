6516ffb0f970ce18013157d0d199b65e095d889c
Mikel Losada <mikel.losada@gmail.com>
2020-06-03 09:28:03 +0200

    Move websocket submission logic to MessageInput and replace vanilla JS events with React ones

--- mysite/frontend/src/components/App.jsx
+++ mysite/frontend/src/components/App.jsx
@@ -4,57 +4,52 @@ import MessageInput from './MessageInput';
 import Message from './Message';
 
 
+function getSocket() {
+  const roomName = window.location.pathname.substr(1);
+  const socketPath = `ws://${
+    window.location.host
+  }/ws/${
+    roomName}`;
+
+  const chatSocket = new WebSocket(
+    socketPath,
+  );
+
+  return chatSocket;
+}
+
+
 class App extends Component {
   constructor(props) {
     super(props);
-   
+
     this.state = {
       messages: [],
     };
+
+    this.chatSocket = getSocket();
   }
 
+
   componentDidMount() {
-    const roomName = location.pathname.substr(1);
-
-    const socketPath = `ws://${
-      window.location.host
-    }/ws/${
-      roomName}`;
-
-    const chatSocket = new WebSocket(
-      socketPath,
-    );
-
-    chatSocket.onmessage = (e) => {
+    this.chatSocket.onmessage = (e) => {
       const data = JSON.parse(e.data);
       const message = { text: data.message, date: data.utc_time };
       message.date = moment(message.date).local().format('YYYY-MM-DD HH:mm:ss');
 
       this.setState((prevState) => ({ messages: prevState.messages.concat(message) }));
     };
-
-    //document.querySelector('#chat-message-input').focus();
-
-    document.querySelector('#chat-message-submit').onclick = () => {
-      const messageInputDom = document.querySelector('#chat-message-input');
-      const message = messageInputDom.value;
-
-      chatSocket.send(JSON.stringify({
-        message,
-      }));
-      messageInputDom.value = '';
-    };
   }
 
   render() {
     const { messages } = this.state;
+    const messageInput = <MessageInput socket={this.chatSocket} />;
 
-    const messageInput = <MessageInput />;
     const messageList = (
       <div>
         {messages.map((item) => (
           <div key={item.id}>
-            <Message key={item.id} text={item.text} date={item.date} />
+            <Message text={item.text} date={item.date} />
           </div>
         ))}
       </div>
--- mysite/frontend/src/components/MessageInput.jsx
+++ mysite/frontend/src/components/MessageInput.jsx
@@ -1,15 +1,47 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 
-function MessageInput() {
+function MessageInput(props) {
+  const messageInput = useRef(null);
+  const { socket } = props;
+
+  function focusInput() {
+    messageInput.current.focus();
+  }
+
+  function resetInput() {
+    messageInput.current.value = '';
+  }
+
+  function sendMessage() {
+    const message = messageInput.current.value;
+    socket.send(JSON.stringify({
+      message,
+    }));
+  }
+
+  function handleSubmission() {
+    sendMessage();
+    resetInput();
+  }
+
+  useEffect(() => {
+    focusInput();
+  });
+
   return (
     <div>
-      <textarea id="chat-message-input" type="text" cols="100" />
+      <textarea id="chat-message-input" type="text" cols="100" ref={messageInput} />
       <br />
-      <input id="chat-message-submit" type="button" className="button" value="Send" />
+      <input id="chat-message-submit" type="button" className="button" value="Send" onClick={handleSubmission} />
     </div>
   );
 }
+
+MessageInput.propTypes = {
+  socket: PropTypes.instanceOf(WebSocket).isRequired,
+};
 
 
 export default MessageInput;
